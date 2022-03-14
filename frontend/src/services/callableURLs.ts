7ca789a5b643e065bace6a04212f51adb10f1c23
Hanxi Liu <z5326602@ad.unsw.edu.au>
2022-03-14 23:53:32 +0800

    OT9900-91 the user registers on the page which can call the api to store the user information

--- frontend/src/services/callableURLs.ts
+++ frontend/src/services/callableURLs.ts
@@ -1,3 +1,4 @@
 const BASE_URL = 'https://australia-southeast1-daydayup-9900.cloudfunctions.net/rs';
 export const getCollectionListApi = `${BASE_URL}/v1/get?col=collections`;
 export const LoginApiURL = `${BASE_URL}/v1/auth/login`;
+export const RegisterApiURL = `${BASE_URL}/v1/auth/signup`;
