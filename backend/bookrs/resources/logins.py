4edc8ea6549441ad08034ef9f4573196fdd80539
Gauraw Rijal <g.rijal@neura.edu.au>
2022-03-15 13:04:23 +1100

    OT9900-10 Change login credentials to email+password instead of username+password

--- backend/bookrs/resources/logins.py
+++ backend/bookrs/resources/logins.py
@@ -12,16 +12,16 @@ class Login(Resource):
     def post(self):
         auth = request.get_json()
 
-        if not auth or not auth.get('username') or not auth.get('password'):
-          return make_response(jsonify({"error" : "Error! Invalid username or password!"}), 401)
+        if not auth or not auth.get('email') or not auth.get('password'):
+          return make_response(jsonify({"error" : "Error! Invalid email or password!"}), 401)
         
-        account = Reader.query.filter_by(username=auth.get('username')).first()
+        account = Reader.query.filter_by(email=auth.get('email')).first()
 
         if not account:
-          return make_response(jsonify({"error" : "Error! Invalid username or password!"}), 401)
+          return make_response(jsonify({"error" : "Error! Invalid email or password!"}), 401)
 
         if account.check_password(auth.get('password')):
           token = create_access_token(identity=str(account.id), expires_delta=datetime.timedelta(days=1))
           return make_response(jsonify({"token": token}), 200)
 
-        return make_response(jsonify({"error" : "Error! Invalid username or password!"}), 401)
+        return make_response(jsonify({"error" : "Error! Invalid email or password!"}), 401)
