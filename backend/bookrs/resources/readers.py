3dc949339db084b608bcb648ca38e9d676b75c71
Hanxi Liu <z5326602@ad.unsw.edu.au>
2022-03-28 11:44:43 +0800

    OT9900-98 check emtpy fields

--- backend/bookrs/resources/readers.py
+++ backend/bookrs/resources/readers.py
@@ -2,6 +2,7 @@ from flask import Blueprint, request, make_response, jsonify
 from flask_restful import Resource
 
 from bookrs.model.reader import Reader, readers_schema, reader_schema, reader_creating_schema
+from bookrs.utils.common import InvalidUsage
 
 readers_bp = Blueprint('readers', __name__)
 
@@ -13,6 +14,10 @@ class Readers(Resource):
 
     def post(self):
         data = request.get_json()
+
+        if len(data.get('username')) == 0 or len(data.get('email')) == 0 or len(data.get('password')) == 0:
+            raise InvalidUsage('Username, Email or Password are not allowed to be empty!', status_code=403)
+
         reader = reader_creating_schema.load(data)
         result = reader_creating_schema.dump(reader.create())
 
