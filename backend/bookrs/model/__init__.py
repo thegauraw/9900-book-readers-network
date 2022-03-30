138d6285a871492bdc1ba01ca5376599576ebf6b
Gauraw Rijal <g.rijal@neura.edu.au>
2022-03-30 15:21:35 +1100

    Add base model which defines common fields and methods for all the models and all models inherit from this

--- backend/bookrs/model/__init__.py
+++ backend/bookrs/model/__init__.py
@@ -5,3 +5,47 @@ from flask_marshmallow import Marshmallow
 db = SQLAlchemy()
 migrate = Migrate()
 ma = Marshmallow()
+
+from sqlalchemy.exc import IntegrityError, SQLAlchemyError
+
+class BaseModel(db.Model):
+  """Define the base model for all other models."""
+  __abstract__ = True
+  id = db.Column(db.Integer(), primary_key=True)
+  created_on = db.Column(db.DateTime(), server_default=db.func.now(),
+                          nullable=False)
+  updated_on = db.Column(db.DateTime(),nullable=False,
+                          server_default=db.func.now(),
+                          onupdate=db.func.now())
+
+  def save(self):
+    """Save an instance of the model from the database."""
+    try:
+      db.session.add(self)
+      db.session.commit()
+      return True
+    except IntegrityError:
+      db.session.rollback()
+      return False
+    except SQLAlchemyError:
+      db.session.rollback()
+      return False
+
+  def update(self):
+    """Update an instance of the model from the database."""
+    try:
+      db.session.commit()
+      return True
+    except SQLAlchemyError:
+      db.session.rollback()
+      return False
+
+  def delete(self):
+    """Delete an instance of the model from the database."""
+    try:
+      db.session.delete(self)
+      db.session.commit()
+      return True
+    except SQLAlchemyError:
+      db.session.rollback()
+      return False
