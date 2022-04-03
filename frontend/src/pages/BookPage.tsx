682617ab290aa20daa5d090b3de861b99154fd02
Yixi-UNSW <z5337291@ad.unsw.edu.au>
2022-04-03 15:24:28 +1000

    Update book page

--- frontend/src/pages/BookPage.tsx
+++ frontend/src/pages/BookPage.tsx
@@ -1,19 +1,14 @@
 import { FC, useContext, useEffect } from 'react';
-import { Typography } from '@mui/material';
 import { useNavigate, useParams } from 'react-router-dom';
 import isEmpty from 'lodash/isEmpty';
 import BookRatingReview from '../components/BookRatingReview';
-import LoadingIndicator from '../components/LoadingIndicator';
 import { Appctx } from '../utils/LocalContext';
-import { getReadingByBookIdForOwner } from '../services/readingAPIs';
 import { NotFoundPath } from '../config/paths';
 
 const BookPage: FC = () => {
   const context = useContext(Appctx);
   let navigate = useNavigate();
   const { bookId } = useParams();
-  const { ownedReadingByBookId, setOwnedReadingByBookId, token } = context;
-  const { settlement, isLoading, error } = ownedReadingByBookId;
   //TODO: Add the book detail
   //TODO: Add "Mark it as read" button
 
