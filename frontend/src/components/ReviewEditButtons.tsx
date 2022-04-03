8330c7c1e04220bde1c0e57888c0bc0442ee969e
Yixi-UNSW <z5337291@ad.unsw.edu.au>
2022-04-04 00:49:51 +1000

    Add a dialog when the user wants to delete a review

--- frontend/src/components/ReviewEditButtons.tsx
+++ frontend/src/components/ReviewEditButtons.tsx
@@ -1,5 +1,13 @@
-import { FC } from 'react';
-import { Box, Button } from '@mui/material';
+import { FC, useState } from 'react';
+import {
+  Box,
+  Button,
+  Dialog,
+  DialogTitle,
+  DialogContent,
+  DialogContentText,
+  DialogActions,
+} from '@mui/material';
 interface ReviewEditButtonsProps {
   inEditing: boolean;
   onCancel: () => void;
@@ -14,6 +22,16 @@ const ReviewEditButtons: FC<ReviewEditButtonsProps> = ({
   onDelete,
   onEdit,
 }) => {
+  const [open, setOpen] = useState(false);
+
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
   return (
     <Box
       sx={{
@@ -45,13 +63,49 @@ const ReviewEditButtons: FC<ReviewEditButtonsProps> = ({
             sx={{
               mr: 2,
             }}
-            onClick={onDelete}
+            onClick={handleClickOpen}
           >
             Delete
           </Button>
           <Button variant="contained" onClick={onEdit}>
             Edit
           </Button>
+          <Dialog open={open} onClose={handleClose}>
+            <DialogTitle id="alert-dialog-title">{'Delete this rating and review'}</DialogTitle>
+            <DialogContent>
+              <DialogContentText id="alert-dialog-description">
+                Are you sure you want to delete this rating and review? You will not be able to
+                recover it.
+              </DialogContentText>
+            </DialogContent>
+            <DialogActions
+              sx={{
+                pb: 2,
+              }}
+            >
+              <Button
+                variant="outlined"
+                sx={{
+                  mr: 2,
+                  p: 1,
+                }}
+                onClick={onDelete}
+              >
+                Yes
+              </Button>
+              <Button
+                variant="contained"
+                onClick={handleClose}
+                autoFocus
+                sx={{
+                  mr: 2,
+                  p: 1,
+                }}
+              >
+                No
+              </Button>
+            </DialogActions>
+          </Dialog>
         </>
       )}
     </Box>
