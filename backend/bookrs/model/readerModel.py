62b51b263e7bd7359c10778dc49bca9fd8c03b63
Hanxi Liu <z5326602@ad.unsw.edu.au>
2022-04-04 23:58:52 +0800

    OT9900-127 Setup back-end to allow to get and update reader profile data

--- backend/bookrs/model/readerModel.py
+++ backend/bookrs/model/readerModel.py
@@ -1,3 +1,4 @@
+from enum import unique
 import re
 
 from marshmallow import fields, pre_load
@@ -9,6 +10,7 @@ from bookrs.model import db, ma
 from bookrs.model.collection import Collection
 from bookrs.utils.exceptions import EmailRegisteredException, EmailFormatException, UsernameRegisteredException
 from .readingModel import ReadingModel
+
 class ReaderModel(db.Model):
     __tablename__ = 'readers'
     id = db.Column(db.Integer, primary_key=True)
@@ -16,6 +18,8 @@ class ReaderModel(db.Model):
     email = db.Column(db.String(120), unique=True, nullable=False)
     password_hash = db.Column(db.String(120), nullable=False)
     status = db.Column(db.Boolean)
+    gender = db.Column(db.String(5))
+    age = db.Column(db.Integer)
     collections = db.relationship("Collection", backref="owner")
     readings = relationship(ReadingModel, backref=backref('reader'))
 
@@ -24,10 +28,12 @@ class ReaderModel(db.Model):
         db.session.commit()
         return self
 
-    def __init__(self, username, email, password_hash):
+    def __init__(self, username, email, password_hash, gender="Male", age=18):
         self.username = username
         self.email = email
         self.password_hash = password_hash
+        self.gender = gender
+        self.age = age
 
         self.check_username(username)
         self.check_email(email)
@@ -54,7 +60,7 @@ class ReaderModel(db.Model):
 
 class ReaderSchema(ma.Schema):
     class Meta:
-        fields = ("id", "username", "email", "status")
+        fields = ("id", "username", "email", "status", "gender", "age")
         model = ReaderModel
 
 reader_schema = ReaderSchema()
@@ -70,6 +76,8 @@ class ReaderCreatingSchema(ma.SQLAlchemySchema):
     username = fields.Str(required = True)
     email = fields.Str(required = True)
     password_hash = fields.Str(load_only = True)
+    gender = fields.Str()
+    age = fields.Integer()
 
     @pre_load
     def process_password(self, user, many, **kwargs):
