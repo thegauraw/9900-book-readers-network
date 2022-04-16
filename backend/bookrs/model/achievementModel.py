f15d951cf724e2f71f86f459ea05fd9e5a85dc66
Hanxi Liu <z5326602@ad.unsw.edu.au>
2022-04-16 11:45:40 +0800

    OT9900-143 update achievement model

--- backend/bookrs/model/achievementModel.py
+++ backend/bookrs/model/achievementModel.py
@@ -1,34 +1,25 @@
-import copy
-
-from marshmallow import fields, post_dump, pre_load, validate
+from marshmallow import fields
 from bookrs.model import BaseModel, db, ma
 from marshmallow_sqlalchemy import auto_field
-from sqlalchemy.orm import relationship, backref
-
-from .readingModel import ReadingModel, ReadingSchema
-# from .badgeModel import BadgeModel
 
 class AchievementModel(BaseModel):
-  __tablename__ = 'achievement'
+  __tablename__ = 'achievements'
   id = db.Column(db.Integer, primary_key=True)
   reader_id = db.Column(db.Integer, db.ForeignKey("readers.id"), nullable=False)
-  badge_id = db.Column(db.Integer(), db.ForeignKey("badge.id"), primary_key=True, nullable=False)
-  # badge = db.relationship(BadgeModel, backref='badge')
-  # readings = relationship(ReadingModel, backref=backref('readings'))
-
-  # @staticmethod
-  # def on_insert():
-  #   db.event.listen(lambda)
+  image = db.Column(db.String(20), unique=True)
+  description = db.Column(db.String())
 
 
 class AchievementSchema(ma.SQLAlchemyAutoSchema):
-    class Meta:
-        model = AchievementModel
-        load_instance = True
-        include_fk = True  
+  class Meta:
+    fields = ('id', 'image', 'description', 'reader_id')
+    model = AchievementModel
+    load_instance = True
 
-    id = auto_field()
-
+  id = auto_field()
+  # image = fields.Str(required = True)
+  # description = fields.Str(required = False)
+  # reader_id = fields.Integer(required = True)
 
 achievement_schema = AchievementSchema()
 achievements_schema = AchievementSchema(many=True)
