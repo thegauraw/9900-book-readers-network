4931a1f20c83f9a42aa5a5c5df280ed432f6a170
Hanxi Liu <z5326602@ad.unsw.edu.au>
2022-04-17 01:50:15 +0800

    OT9900-146 create recommendation API

--- backend/bookrs/__init__.py
+++ backend/bookrs/__init__.py
@@ -14,6 +14,7 @@ from .resources.collections import collections_bp
 from .resources.books import books_bp
 from .resources.search import search_bp
 from .resources.achievements import achievements_bp
+from .resources.recommendation import recommendation_bp
 from .resources.readings import Readings, ReadingsByBookId, readings_bp
 from .resources.owned_readings import OwnedReadingByBookId, owned_readings_bp
 from .resources.goals import goals_bp, Goals
@@ -91,6 +92,8 @@ def create_app(test_config=None):
     app.register_blueprint(search_bp)
 
     app.register_blueprint(achievements_bp)
+
+    app.register_blueprint(recommendation_bp)
 
     @app.errorhandler(InvalidUsage)
     def handle_invalid_usage(error):
---
+++ backend/bookrs/resources/recommendation.py
@@ -0,0 +1,47 @@
+import copy
+import json
+import requests
+
+from flask import Blueprint, request
+from flask_restful import Resource
+from bookrs.model.bookModel import book_details_schema, BookModel
+from bookrs.resources import api
+from bookrs.utils.common import SUCCESS
+from bookrs.utils.exceptions import InvalidSearchException
+from bookrs.services.filters import filterBooksWithRating
+from bookrs.third_party.googleAPIs import seach_books_from_google_with_filter_mode
+
+recommendation_bp = Blueprint('recommendation', __name__)
+
+
+class RecommendingBooks(Resource):
+
+  def get(self):
+    """
+      search and filter books `GET /search?q=<string: q>&startIndex=<int: startIndex>&mode=<string: mode>&maxResults=<int: maxResults>&minRating=<float: minRating>`
+      mode includes: title, author, category and publisher
+    """
+
+    query_string = request.args.get('q') if request.args.get('q') else None
+    startIndex = request.args.get('startIndex') if request.args.get('startIndex') else 0
+    mode = request.args.get('m') if request.args.get('m') else 'title'
+    maxResults = request.args.get('maxResults') if request.args.get('maxResults') else 5
+    minRating = float(request.args.get('minRating')) if request.args.get('minRating') else 0.0 #for advanced filter
+
+    # Search for google book api
+    # For each result, add book detail to the response
+
+    try:
+      # search books with google api
+      data = seach_books_from_google_with_filter_mode(query=query_string,
+                                                      mode=mode,
+                                                      startIndex=startIndex,
+                                                      maxResults=maxResults)
+      # filtered_data = filterBooksWithRating(minRating=minRating, data=data)
+
+      return SUCCESS(payload=data)
+    except Exception as e:
+      raise e
+
+
+api.add_resource(RecommendingBooks, '/recommendation')
