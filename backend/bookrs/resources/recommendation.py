2da1cc02417a369665c0e0459d97bfe4a6f95d3a
Hanxi Liu <z5326602@ad.unsw.edu.au>
2022-04-17 02:00:01 +0800

    OT9900-146 remove unused import

--- backend/bookrs/resources/recommendation.py
+++ backend/bookrs/resources/recommendation.py
@@ -1,14 +1,7 @@
-import copy
-import json
-import requests
-
 from flask import Blueprint, request
 from flask_restful import Resource
-from bookrs.model.bookModel import book_details_schema, BookModel
 from bookrs.resources import api
 from bookrs.utils.common import SUCCESS
-from bookrs.utils.exceptions import InvalidSearchException
-from bookrs.services.filters import filterBooksWithRating
 from bookrs.third_party.googleAPIs import seach_books_from_google_with_filter_mode
 
 recommendation_bp = Blueprint('recommendation', __name__)
@@ -28,15 +21,14 @@ class RecommendingBooks(Resource):
     maxResults = request.args.get('maxResults') if request.args.get('maxResults') else 5
     minRating = float(request.args.get('minRating')) if request.args.get('minRating') else 0.0 #for advanced filter
 
-    # Search for google book api
-    # For each result, add book detail to the response
-
     try:
-      # search books with google api
+      # recommend books with google api
       data = seach_books_from_google_with_filter_mode(query=query_string,
                                                       mode=mode,
                                                       startIndex=startIndex,
                                                       maxResults=maxResults)
+
+      # TODO: comment out this if there is a need to filter with minimum average rating
       # filtered_data = filterBooksWithRating(minRating=minRating, data=data)
 
       return SUCCESS(payload=data)
