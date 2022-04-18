ceb637a8a6ec823407ebcea3eca335b8b65b5e6f
Zachary Zhang <zachary97@foxmail.com>
2022-04-19 01:04:51 +0800

    1. Update the table structure of collectedbooks 2. Finished recent 10 users

--- backend/bookrs/resources/books.py
+++ backend/bookrs/resources/books.py
@@ -1,12 +1,13 @@
 from flask import Blueprint, abort, request
 from flask_jwt_extended import get_jwt_identity, jwt_required
 from flask_restful import Resource
-from bookrs.model.bookModel import BookModel, book_details_schema, books_details_schema
+from bookrs.model.bookModel import BookModel, Collectedbooks, book_details_schema, books_details_schema, collected_book_schema, collected_books_schema
 from bookrs.model.collection import Collection
 from bookrs.resources import api
 from bookrs.utils.common import SUCCESS
 from bookrs.utils.exceptions import BookCollectException, BookCreateException, BookDropException
 from bookrs.third_party.googleAPIs import get_book_details_from_google
+from datetime import datetime
 
 books_bp = Blueprint('books', __name__)
 
@@ -40,11 +41,18 @@ class CollectedBooks(Resource):
       if not book.save():
         raise BookCreateException()
 
-    collection.books.append(book)
-    if collection.update():
+    # collection.books.append(book)
+    # if collection.update():
       # TODO: add record to book_readings as well
       # show list of books in the collection - just like get
-      books_data_dump = books_details_schema.dump(collection.books)
+    books_data_dump = books_details_schema.dump(collection.books)
+    collected_data = Collectedbooks.query.filter_by(collection_id=collection_id, book_id=book.id).first()
+    if collected_data:
+      raise BookCollectException()
+    collected_book = {"collection_id": collection_id,
+                      "book_id": book.id,
+                      "collected_date": datetime.now().strftime('%Y-%m-%d %H:%M:%S')}
+    if collected_book_schema.load(collected_book).create():
       return SUCCESS(message='Book added to the collection successfully', payload=books_data_dump)
     else:
       raise BookCollectException()
@@ -61,12 +69,12 @@ class CollectedBook(Resource):
     # check if the user owns the collection
     current_user = get_jwt_identity()
     collection = Collection.get_by_reader_and_id(reader_id=current_user, id=collection_id)
-
     book = BookModel.query.filter_by(volume_id=volume_id).first()
-
     if book in collection.books:
-      collection.books.remove(book)
-      if collection.update(): # db-commit
+      # collection.books.remove(book)
+      collected_data = Collectedbooks.query.filter_by(collection_id=collection_id, book_id=book.id).first()
+      # if collection.update(): # db-commit
+      if collected_data.delete(): # db-commit
         return SUCCESS(message='Book removed from the collection successfully')
       else:
         raise BookDropException()
--- backend/bookrs/resources/readers.py
+++ backend/bookrs/resources/readers.py
@@ -3,13 +3,17 @@ import copy
 from flask import Blueprint, request
 from flask_restful import Resource
 
-from bookrs.model.readerModel import ReaderModel, readers_schema, reader_creating_schema
+from bookrs.model.readerModel import ReaderModel, readers_schema, reader_creating_schema, reader_schema
+from bookrs.model.bookModel import BookModel, Collectedbooks, book_details_schema, collected_book_schema, collected_books_schema
+from bookrs.model.collection import Collection, collection_schema
 from bookrs.resources import api
 from bookrs.utils.exceptions import NullRegisterFieldException, CollectionCreateException
 from bookrs.utils.common import SUCCESS
 from bookrs.model.collection import collection_schema
+from sqlalchemy import desc
 
 readers_bp = Blueprint('readers', __name__)
+
 
 class Readers(Resource):
 
@@ -43,4 +47,23 @@ class Readers(Resource):
         return SUCCESS(payload=result, status_code=201)
 
 
+class RecentReaders(Resource):
+    def get(self, volume_id):
+        book_data = BookModel.query.filter_by(volume_id=volume_id).first()
+        book = book_details_schema.dump(book_data)
+        collected_data = Collectedbooks.query.filter_by(book_id=book.get("id")).order_by(desc("collected_date")).limit(10).all()
+        collected_list = collected_books_schema.dump(collected_data)
+        res_list = []
+        for reading in collected_list:
+            collection = Collection.query.filter_by(id=reading.get("collection_id")).first()
+            reader_id = collection_schema.dump(collection).get("reader_id")
+            reader = ReaderModel.query.filter_by(id=reader_id).first()
+            user_name = reader_schema.dump(reader).get("username")
+            reader_data = {"reader_id": reader_id,
+                           "user_name": user_name}
+            res_list.append(reader_data)
+        return SUCCESS(payload=res_list)
+
+
 api.add_resource(Readers, '/readers')
+api.add_resource(RecentReaders, '/recentreaders/<string:volume_id>')
