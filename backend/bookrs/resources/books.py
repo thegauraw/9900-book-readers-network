724c435ba389011fd0ebbc2ca044f785772bc74c
Zachary Zhang <zachary97@foxmail.com>
2022-04-19 02:43:58 +0800

    Included OT9900-37-the-10-most-recently-added-books-by-a-reader

--- backend/bookrs/resources/books.py
+++ backend/bookrs/resources/books.py
@@ -2,12 +2,13 @@ from flask import Blueprint, abort, request
 from flask_jwt_extended import get_jwt_identity, jwt_required
 from flask_restful import Resource
 from bookrs.model.bookModel import BookModel, Collectedbooks, book_details_schema, books_details_schema, collected_book_schema, collected_books_schema
-from bookrs.model.collection import Collection
+from bookrs.model.collection import Collection, collection_schema, collections_schema
 from bookrs.resources import api
 from bookrs.utils.common import SUCCESS
 from bookrs.utils.exceptions import BookCollectException, BookCreateException, BookDropException
 from bookrs.third_party.googleAPIs import get_book_details_from_google
 from datetime import datetime
+from sqlalchemy import desc
 
 books_bp = Blueprint('books', __name__)
 
@@ -83,3 +84,33 @@ class CollectedBook(Resource):
 
 api.add_resource(CollectedBooks, '/collections/<int:collection_id>/books')
 api.add_resource(CollectedBook, '/collections/<int:collection_id>/books/<string:volume_id>')
+
+
+class RecentBooks(Resource):
+  decorators = [jwt_required()]
+
+  def get(self, reader_id):
+    if reader_id == 0:
+      reader_id = get_jwt_identity()
+    collections_data = Collection.query.filter_by(reader_id=reader_id).all()
+    collections = collections_schema.dump(collections_data)
+    collection_ids = []
+    for collection in collections:
+      collection_ids.append(collection.get("id"))
+    collection_num = len(collection_ids)
+    res_list = []
+    if collection_num:
+      collected_data = Collectedbooks.query.filter(Collectedbooks.collection_id.in_(collection_ids)).order_by(desc("collected_date")).limit(10).all()
+      collecteds = collected_books_schema.dump(collected_data)
+      for collected in collecteds:
+        book_data = BookModel.query.filter_by(id=collected.get("book_id")).first()
+        book = book_details_schema.dump(book_data)
+        book_info = {"volume_id": book.get("volume_id"),
+                     "title": book.get("title"),
+                     "smallThumbnail": book.get("smallThumbnail")}
+        res_list.append(book_info)
+    return SUCCESS(payload=res_list)
+
+
+api.add_resource(RecentBooks, '/recent_collected_books/<int:reader_id>')
+# api.add_resource(RecentBooksbyId, '/recent_collected_books/<int:reader_id>')
