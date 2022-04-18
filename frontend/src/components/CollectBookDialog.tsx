d5f754d334aa84dc293b0465071f20d7150a6584
Yixi-UNSW <z5337291@ad.unsw.edu.au>
2022-04-18 17:58:28 +1000

    Add collecting button on the book page

---
+++ frontend/src/components/CollectBookDialog.tsx
@@ -0,0 +1,142 @@
+import { FC, useContext, useState } from 'react';
+import { Appctx } from '../utils/LocalContext';
+import {
+  Button,
+  Dialog,
+  DialogTitle,
+  DialogContent,
+  DialogContentText,
+  DialogActions,
+  Box,
+  List,
+  ListItem,
+  ListItemText,
+  Checkbox,
+  Typography,
+  Divider,
+} from '@mui/material';
+import { addBookToCollections } from '../services/bookAPIs';
+import LoadingIndicator from './LoadingIndicator';
+
+const CollectBookDialog: FC = () => {
+  const context = useContext(Appctx);
+  const { collectionList, bookDetails, token } = context;
+  const [checked, setChecked] = useState<string[]>([]);
+  const [open, setOpen] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
+  const [alert, setAlert] = useState('');
+  const handleToggle = (value: string) => () => {
+    const currentIndex = checked.indexOf(value);
+    const newChecked = [...checked];
+    if (currentIndex === -1) {
+      newChecked.push(value);
+    } else {
+      newChecked.splice(currentIndex, 1);
+    }
+    setChecked(newChecked);
+  };
+
+  const onSubmit = () => {
+    (async function () {
+      try {
+        setIsLoading(true);
+        await addBookToCollections(token, checked, bookDetails?.settlement?.id ?? '');
+      } catch (error) {
+        setAlert(error as string);
+      } finally {
+        setIsLoading(false);
+        setOpen(false);
+      }
+    })();
+  };
+
+  const onClose = () => {
+    setOpen(false);
+  };
+
+  const customList = () => (
+    <Box sx={{ width: 200, height: 230, overflow: 'auto' }}>
+      <List dense component="div" role="list">
+        {collectionList?.settlement?.map((collection) => {
+          return (
+            <ListItem
+              key={collection.id}
+              role="listitem"
+              button
+              onClick={handleToggle(collection.id)}
+            >
+              <Checkbox
+                checked={checked.indexOf(collection.id) !== -1}
+                tabIndex={-1}
+                disableRipple
+              />
+              <ListItemText id={collection.id} primary={`${collection.title}`} />
+            </ListItem>
+          );
+        })}
+        <ListItem />
+      </List>
+    </Box>
+  );
+
+  const toCollect = () => (
+    <Dialog open={open} onClose={onClose}>
+      <DialogTitle>{`Add the book to my collection(s)`}</DialogTitle>
+      <DialogContent sx={{ mt: 3, mx: 2, px: 3 }}>
+        <Typography variant="subtitle1">My Collections</Typography>
+        <Divider />
+        <DialogContentText>
+          {isLoading ? (
+            <LoadingIndicator />
+          ) : alert ? (
+            <Typography variant="subtitle1">{alert}</Typography>
+          ) : (
+            customList()
+          )}
+        </DialogContentText>
+      </DialogContent>
+      <DialogActions
+        sx={{
+          pb: 2,
+        }}
+      >
+        <Button
+          variant="outlined"
+          sx={{
+            mr: 2,
+            p: 1,
+          }}
+          onClick={onClose}
+        >
+          Cancel
+        </Button>
+        <Button
+          variant="contained"
+          onClick={onSubmit}
+          autoFocus
+          sx={{
+            mr: 2,
+            p: 1,
+          }}
+        >
+          Submit
+        </Button>
+      </DialogActions>
+    </Dialog>
+  );
+
+  return (
+    <>
+      <Button
+        variant="contained"
+        sx={{ mr: 1, mt: 2, width: '200px' }}
+        onClick={() => setOpen(true)}
+      >
+        Collect the book
+      </Button>
+      {toCollect()}
+    </>
+  );
+};
+
+export default CollectBookDialog;
