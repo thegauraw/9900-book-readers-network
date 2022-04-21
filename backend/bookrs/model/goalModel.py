a9370f55df2ad52fc45a1ef58a1c4c8700d980a4
Zachary Zhang <zachary97@foxmail.com>
2022-04-21 09:52:43 +0800

    Complete comments, delete redundant contents

--- backend/bookrs/model/goalModel.py
+++ backend/bookrs/model/goalModel.py
@@ -1,9 +1,5 @@
-import re
-
-from marshmallow import fields, pre_load
-from werkzeug.security import check_password_hash, generate_password_hash
+from marshmallow import fields
 from marshmallow_sqlalchemy import auto_field
-
 from bookrs.model import db, ma
 
 class GoalModel(db.Model):
@@ -11,43 +7,32 @@ class GoalModel(db.Model):
 
     id = db.Column(db.Integer, primary_key=True)
     userid = db.Column(db.String(80), db.ForeignKey("readers.id"), nullable=False)
-    month = db.Column(db.String(10), nullable=False)
-    # end_time = db.Column(db.Date, nullable=False)
-    goal_num = db.Column(db.Integer, nullable=False)
-    # finish = db.Column(db.Boolean, default=False)
-    # finished_num = db.Column(db.Integer, default=0)
+    month = db.Column(db.String(10), nullable=False)  # "yyyy-mm" format string to make it intuitive
+    goal_num = db.Column(db.Integer, nullable=False)  # number of books to read in a month
 
     def create(self):
         db.session.add(self)
         db.session.commit()
         return self
 
-    """def delete(self):
-        db.session.delete(self)
-        db.session.commit()
-        return self"""
-
     def update(self):
         db.session.commit()
         return self
 
     def __repr__(self):
-         return f'<Goal {self.id} { "finished" if self.status else "in progress" }>'
+        return f'<Goal {self.id} { "finished" if self.status else "in progress" }>'
 
 
 class GoalSchema(ma.SQLAlchemySchema):
     class Meta(ma.SQLAlchemySchema.Meta):
-       model = GoalModel
-       load_instance = True
-       include_fk = True
+        model = GoalModel
+        load_instance = True
+        include_fk = True
 
-    id = auto_field() #fields.Number(dump_only=True)
+    id = auto_field()
     userid = fields.Int(required=True)
     goal_num = fields.Int(required=True)
     month = fields.Str(required=True)
-    # end_time = fields.Date(required=True)
-    # finish = fields.Bool(default=False)
-    # finished_num = fields.Int(default=0)
 
 
 goal_schema = GoalSchema()
--- backend/bookrs/resources/goals.py
+++ backend/bookrs/resources/goals.py
@@ -1,17 +1,16 @@
 from flask import Blueprint, request, make_response, jsonify
 from flask_restful import Resource
+from flask_jwt_extended import get_jwt_identity, jwt_required
 
 from bookrs.model.goalModel import GoalModel, goal_schema, goals_schema
 from bookrs.resources import api
 from bookrs.model.readingModel import ReadingModel, reading_schema, readings_schema
 from bookrs.model.bookModel import BookModel, book_details_schema
+from bookrs.utils.common import SUCCESS
 
-from flask_jwt_extended import get_jwt_identity, jwt_required
 from datetime import datetime
 from dateutil.relativedelta import relativedelta
-
 from sqlalchemy import desc, and_, extract
-from bookrs.utils.common import SUCCESS
 
 goals_bp = Blueprint('goals', __name__)
 
@@ -21,38 +20,43 @@ class Goals(Resource):
 
     # get the data of goals
     def get(self):
-        goals_list = []
         current_user = get_jwt_identity()
-
+        # retrieve the earliest record of goals (first setting)
         first_data = GoalModel.query.filter_by(userid=current_user).order_by("month").first()
         first_goal = goal_schema.dump(first_data)
-        
+        # user has set the goal
         if first_goal:
+            goals_list = []  # initialize return list
             goal_num = first_goal.get("goal_num")
+            # make datetime as "yyyy-mm-01" type to ensure relativedelta function works correctly
             first_month = datetime.strptime((first_goal.get("month") + "-01"), '%Y-%m-%d')
             now_month = datetime.strptime(datetime.now().strftime("%Y-%m") + "-01", '%Y-%m-%d')
-            # now_month = first_month + relativedelta(months=12)
+            # not use While loop to avoid falling into an infinite loop
             for i in range(1000):
-                current_month = first_month + relativedelta(months=i)
-                month_str = datetime.strftime(current_month, '%Y-%m')
+                current_month = first_month + relativedelta(months=i)  # get monthly data one by one from first setting
+                month_str = datetime.strftime(current_month, '%Y-%m')  # turn back to model format for searching
                 data = GoalModel.query.filter_by(userid=current_user, month=month_str).first()
-                if goal_schema.dump(data):    # have changed
+                if goal_schema.dump(data):    # goal have changed in current month, change the goal number
                     goal_num = goal_schema.dump(data).get("goal_num")
-
+                # retrieve the books that reader read in this month
                 books = ReadingModel.query.filter(and_(
                     extract('year', ReadingModel.last_update_read_at) == current_month.year,
                     extract('month', ReadingModel.last_update_read_at) == current_month.month),
                     ReadingModel.reader_id == current_user, ReadingModel.has_read == True
                 ).order_by(desc("last_update_read_at")).all()
                 books_list = readings_schema.dump(books)
-                read_num = len(books_list)
+                read_num = len(books_list)  # number of books that reader read in this month
+                # user has finished the goal
                 if read_num >= goal_num:
                     finish = True
+                    # finish date is earlist date when user has read enough books, not the latest date to read the book in this month
                     finish_date = (books_list[-goal_num]).get("last_update_read_at") if goal_num > 0 else datetime.strftime(current_month, '%Y-%m-%d')
                 else:
                     finish = False
                     finish_date = None
+                # initialize return book list
                 book_list = []
+                # retrieve concise book information
                 for book in books_list:
                     if book.get("volume_id"):
                         book_data = BookModel.query.filter_by(volume_id=book.get("volume_id")).first()
@@ -62,6 +66,7 @@ class Goals(Resource):
                                            "title": book_info.get("title"),
                                            "smallThumbnail": book_info.get("smallThumbnail")}
                             book_list.append(book_detail)
+                # goal information for current month
                 goal = {"month": month_str,
                         "goal_num": goal_num,
                         "read_num": read_num,
@@ -69,11 +74,12 @@ class Goals(Resource):
                         "finish_date": finish_date,
                         "book_list": book_list.copy()}
                 goals_list.append(goal)
-                if current_month == now_month:
+                if current_month == now_month:  # have retrieved all goals since first setting
                     break
+            # Sort the reading goal by recent months for better UX
             goals_list.sort(key=lambda i: i['month'], reverse=True)
             return SUCCESS(payload=goals_list)
-        else:
+        else:  # user has not set the goal
             return SUCCESS(payload=[])
 
     # add goal
@@ -81,7 +87,8 @@ class Goals(Resource):
         data = request.get_json()
         current_user = get_jwt_identity()
         data["userid"] = current_user
-        if data.get("month"):
+        if data.get("month"):  # should not be sent in normal operation, just send for debugging use
+            # transform datetime to string of model defined style
             data["month"] = datetime.strftime(datetime.strptime(data.get("month"), '%Y-%m-%d'), '%Y-%m')
         else:
             data["month"] = datetime.now().strftime("%Y-%m")
@@ -90,24 +97,12 @@ class Goals(Resource):
         result = goal_schema.dump(goal.create())
         return SUCCESS(payload=result, status_code=201)
 
-    # delete goal
-    """def delete(self):
-        data = request.get_json()
-        goals_list = data.get("goals")
-        delete_list = []
-
-        for goal_id in goals_list:
-            goal = GoalModel.query.filter_by(id=goal_id).first()
-            if goal:
-                goal.delete()
-                delete_list.append(goal_id)
-        return make_response(jsonify({"goals deleted": delete_list}), 201)"""
-
     # revise goal
     def put(self):
         data = request.get_json()
         current_user = get_jwt_identity()
-        if data.get("month"):
+        if data.get("month"):  # should not be sent in normal operation, just send for debugging use
+            # transform datetime to string of model defined style
             date = datetime.strptime(data.get("month"), '%Y-%m-%d')
             month = datetime.strftime(date, '%Y-%m')
         else:
@@ -118,17 +113,11 @@ class Goals(Resource):
             goal.goal_num = data.get("goal_num")
             goal.update()
             result = goal_schema.dump(goal)
-        else:
+        else:  # no existing goal data for this month, create a new record
             data["userid"] = current_user
             data["month"] = month
             goal = goal_schema.load(data)
             result = goal_schema.dump(goal.create())
-        """if data.get("goal_num"):
-            goal.goal_num = data.get("goal_num")
-        if data.get("start_time"):
-            goal.start_time = datetime.strptime(data.get("start_time"), '%Y-%m-%d')
-        if data.get("end_time"):
-            goal.end_time = datetime.strptime(data.get("end_time"), '%Y-%m-%d')"""
         return SUCCESS(payload=result)
 
 
